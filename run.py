1c4dfa1acccaf0c4477b85138fac8b22f27699a7
dmytro.malikin <dmytro.malikin@gmail.com>
2024-12-01 21:16:52 +0200

    Update run

--- run.py
+++ run.py
@@ -1,21 +1,41 @@
+import argparse
 import importlib
+from types import ModuleType
 
 from base.day import BaseDay
 
 
-def main(year: int, day: int, test: bool = False):
+class Error(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+def _import_day(year: int, day: int) -> ModuleType:
     package_name = f"aoc{year}"
     module_name = f"d{day}"
-    module = importlib.import_module(f"{package_name}.{module_name}")
 
-    day: BaseDay
+    try:
+        module = importlib.import_module(f"{package_name}.{module_name}")
+    except ModuleNotFoundError:
+        raise Error(f"Could not find {module_name} module in {package_name} package.")
 
+    return module
+
+
+def _get_day(module: ModuleType, test: bool) -> BaseDay:
     if test:
-        print(f"Initializing Year {year}, Day {day} [TEST] ...")
         day = module.TestDay()
     else:
-        print(f"Initializing Year {year}, Day {day} ...")
         day = module.Day()
+
+    return day
+
+
+def run(year: int, day: int, test: bool = False):
+    module = _import_day(year=year, day=day)
+
+    print(f"Initializing Year {year}, Day {day} {'[TEST]' if test else ''} ...")
+    day = _get_day(module, test=test)
 
     part1_result = day.part1()
     print(f"Part 1 result: {part1_result}")
@@ -24,6 +44,37 @@ def main(year: int, day: int, test: bool = False):
     print(f"Part 2 result: {part2_result}")
 
 
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Process some options.")
+
+    parser.add_argument(
+        "-y",
+        "--year",
+        type=int,
+        help="Specify the year",
+        default=2024,
+    )
+    parser.add_argument("-d", "--day", type=int, help="Specify the day")
+    parser.add_argument(
+        "-t",
+        "--test",
+        action="store_true",
+        help="Enable the test flag (True/False)",
+        default=False,
+    )
+
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    try:
+        run(year=args.year, day=args.day, test=args.test)
+    except Error as e:
+        print(e.message)
+
+
 if __name__ == "__main__":
-    main(2024, 1, test=True)
-    main(2024, 1, test=False)
+    main()
