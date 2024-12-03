a3331f958da843028ed03cdfb462d53a5d763c45
dmytro.malikin <dmytro.malikin@gmail.com>
2024-12-03 21:11:28 +0200

    Day 3, part 2

--- aoc2024/day3.py
+++ aoc2024/day3.py
@@ -10,20 +10,41 @@ class Day(BaseDay):
     __data_path__ = DATA_DIR / "day3.txt"
 
     def part1(self) -> int:
-        regex = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+        regex = "mul\((\d+),(\d+)\)"
+        pattern = re.compile(regex)
+        data = self.merge_lines()
 
         result = 0
+        matches = pattern.findall(data)
 
-        for line in self.data:
-            r = re.findall(regex, line)
-
-            for a, b in r:
-                result += int(a) * int(b)
+        for a, b in matches:
+            result += int(a) * int(b)
 
         return result
 
     def part2(self) -> int:
-        pass
+        regex = "mul\((?P<n1>\d+),(?P<n2>\d+)\)|(?P<on>do\(\))|(?P<off>don't\(\))"
+        pattern = re.compile(regex)
+        data = self.merge_lines()
+
+        result = 0
+        mul_on = True
+        for match in pattern.finditer(data):
+            if match.group("off") is not None:
+                mul_on = False
+
+            elif match.group("on") is not None:
+                mul_on = True
+
+            else:
+                if mul_on:
+                    a, b = match.group("n1"), match.group("n2")
+                    result += int(a) * int(b)
+
+        return result
+
+    def merge_lines(self) -> str:
+        return "".join(self.data)
 
 
 class TestDay(Day):
@@ -34,11 +55,11 @@ if __name__ == "__main__":
     part1_test_result = TestDay().part1()
     print(f"{part1_test_result=}")
 
-    # part2_test_result = TestDay().part2()
-    # print(f"{part1_test_result=}")
+    part2_test_result = TestDay().part2()
+    print(f"{part2_test_result=}")
 
     part1_result = Day().part1()
     print(f"{part1_result=}")
 
-    # part2_result = Day().part2()
-    # print(f"{part2_result=}")
+    part2_result = Day().part2()
+    print(f"{part2_result=}")
